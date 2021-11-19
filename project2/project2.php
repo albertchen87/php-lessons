9d174c14b71ff8185612de38491eaf48f1f46905
albertchen87 <76764838+albertchen87@users.noreply.github.com>
2021-11-19 21:37:27 +0800

    hi

---
+++ project2/project2.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+    <form acton = "index.php" method = POST">
+        <label>Username</label>
+        <input type = "text" name = "username">
+    </form>
+
+    <form action = "index.php" method = "GET">
+        <label>Username</label>
+        <input type = "text" name = "Username">
+    </form>
+</body>
+</html>
