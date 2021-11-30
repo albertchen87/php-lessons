15c00c01a3c7dfed748887af48babe45b5d02daa
albertchen87 <albertchen2022@pacificamerican.org>
2021-12-01 00:22:08 +0800

    hi

--- lesson3/database.php
+++ lesson3/database.php
@@ -1,14 +1,14 @@
 <?php
 
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=wayner_kerr_restaurant","root", "");
+    $conn = new PDO("mysql:host=localhost;dbname=test","root", "");
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 
-    $sql = "INSERT INTO `menu` (Menu_ID, Restaurant_ID, List_ID) VALUES ('2','2','2')";
-    $conn->exec($sql);
-    echo "Inserted successfully";
+    //$sql = "INSERT INTO `menu` (Menu_ID, Restaurant_ID, List_ID) VALUES ('2','2','2')";
+    //$conn->exec($sql);
+    //echo "Inserted successfully";
 
   } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
