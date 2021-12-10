e4052739429a534e96957ec6c14a642b1185f121
albertchen87 <76764838+albertchen87@users.noreply.github.com>
2021-12-10 09:03:38 +0800

    hi

---
+++ project4/messages.php
@@ -0,0 +1,32 @@
+<?php
+
+
+echo "hi";
+$resnum = "1";
+$tablenumber =  $_POST['tablenumber'];
+$item1 = $_POST['item1'];
+$item2 = $_POST['item2'];
+$item3 = $_POST['item3'];
+$item4 = $_POST['item4'];
+$item5 = $_POST['item5'];
+
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=test","root", "");
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+    $sql = "INSERT INTO `orderlist`(Restaurant_ID, TableNumber, Item_1, Item_2, Item_3, Item_4, Item_5) VALUES ('$resnum', '$tablenumber', '$item1', '$item2', '$item3', '$item4', '$item5')";
+    $conn->exec($sql);
+    echo "Inserted successfully";
+
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+  $conn = null;
+
+  echo "<a href=\"project3.php\">Back!</a>";
+
+  ?>
+  
