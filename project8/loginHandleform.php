e0c3b7f3168b356b388105db39ade1545a6eb5bf
albertchen87 <76764838+albertchen87@users.noreply.github.com>
2022-02-12 17:20:37 +0800

    hi

--- project8/loginHandleform.php
+++ project8/loginHandleform.php
@@ -10,35 +10,38 @@
 <body>
     <?php
 
-    $Email = $_POST['Email'];
-    $Password = $_POST['Password'];
+    $Email = addslashes($_POST['Email']);
+    $Password = addslashes($_POST['Password']);
+    echo $Email . '<br>';
     try {
         $conn = new PDO("mysql:host=localhost;dbname=user","root", "");
-        // set the PDO error mode to exception
-        echo "Connected successfully"."<br>";
-      
-        $sql = "SELECT * FROM `user` WHERE `email` LIKE ?";
-        $Email = "%$Email%";
+        // set the PDO error mode to exception      
+        $sql = 'SELECT * FROM `user` WHERE `email` = ?';
+        echo $Email;    
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(1, $Email);
         $stmt->execute();
         echo "executed successfully" . "<br>";
-
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $testPassword = $row['password'];
-        if(password_verify($Password, $testPassword)){
-            echo "Welcome, user" . "<br>";
-            session_start();
-            $_SESSION['Username'] = $row['user'];
-            $_SESSION['Password'] = $_POST['Password'];
-            $_SESSION['Email'] = $_POST['Email'];
-            echo "<a href = 'profile.php'>Profile</a>";
+ 
+        if($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $testPassword = $row['password'];
+            if(password_verify($Password, $testPassword)){
+                echo "Welcome, user" .  "<br>";
+                session_start();
+                $_SESSION['Username'] = $row['user'];
+                $_SESSION['Password'] = $_POST['Password'];
+                $_SESSION['Email'] = $row['email'];
+                echo "<a href = 'profile.php'>Profile</a>";
+            }
+            else {
+                echo "<h1>Email or password is incorrect2</h1>";
+                echo "<a href = 'login.html'>Login</a>";
+            }
         }
         else {
-            echo "<h1>username or password is incorrect</h1>";
+            echo "<h1>Email or password is incorrect1 could not find email</h1>";
             echo "<a href = 'login.html'>Login</a>";
         }
-
       
       } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage() . "<br>";
