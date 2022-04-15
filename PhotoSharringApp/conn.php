852ef473a09ae486762191f55d576fbe9740d780
albertchen87 <76764838+albertchen87@users.noreply.github.com>
2022-04-15 23:16:44 +0800

    hi

--- PhotoSharringApp/conn.php
+++ PhotoSharringApp/conn.php
@@ -1,3 +1,3 @@
 <?php
-$conn = new PDO("mysql:host=localhost;dbname=PhotoSharingApp","root", "");
+$conn = new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_09d14b0105a7fef","b1723d357841e1", "b8a53cc7");
 ?>
